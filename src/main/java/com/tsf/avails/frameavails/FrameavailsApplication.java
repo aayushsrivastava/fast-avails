6e30d87b4f48c172fd72b8e8079b75079cd03775
Priyank Gupta <priyaaank@gmail.com>
2022-02-07 17:06:10 +0530

    Added app skeleton

---
+++ src/main/java/com/tsf/avails/frameavails/FrameavailsApplication.java
@@ -0,0 +1,13 @@
+package com.tsf.avails.frameavails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameavailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameavailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsf/avails/frameavails/FrameavailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsf.avails.frameavails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameavailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
